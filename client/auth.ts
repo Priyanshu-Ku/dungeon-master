e7bbf08e91172f6cf5497a8205b29cdf69b5710f
Priyanshu8023 <priyanshukumar8023@gmail.com>
2026-05-08 19:19:56 +0530

    feat: add Oauth for login

---
+++ client/auth.ts
@@ -0,0 +1,17 @@
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { prisma } from "./lib/prisma";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+});
